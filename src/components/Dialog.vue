16c89c91f0d4aed9d25680726b383517fe7ac913
nyuruN <zhangwai.herbst@gmail.com>
2025-05-18 17:24:37 +0200

    feature: Show Chapter Navigation on Hover
    - Fix incorrect file button size

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -48,20 +48,27 @@ async function loadEpub(e: Event) {
 }
 async function next() { let p = chapter.value?.next(); if (p) { chapter.value = await p; await nextTick(); chapter.value.resetCaret() } }
 async function prev() { let p = chapter.value?.prev(); if (p) { chapter.value = await p; await nextTick(); chapter.value.resetCaret() } }
+async function toChapter(n: number) { let c = book.value?.getChapter(n); if (c) { chapter.value = await c; await nextTick(); chapter.value.resetCaret() } }
 </script>
 
 <template>
     <div id="container">
         <!-- <h1>{{ book ? book.title : 'Choose a file' }}</h1> -->
         <div id="options">
-            <button class="button active-border">
+            <button id="toc" class="button active-border" :disabled="!book">
                 <font-awesome-icon :icon="['fas', 'bars']" fixed-width />
+                <div v-if="book" id="toc-content">
+                    <div v-for="( arr, index ) in book?.toc" @click="toChapter(index)">
+                        {{ arr[0] }}
+                    </div>
+                </div>
             </button>
-            <label id="file-label" class="button active-border" for="file-input">
+            <button class="button active-border" onclick="document.getElementById('file-input').click();">
                 <font-awesome-icon :icon="['fas', 'folder-open']" fixed-width />
-                <input id='file-input' type="file" accept=".epub" @change="loadEpub" style="display: none;"/>
-            </label>
+                <input id='file-input' type="file" accept=".epub" @change="loadEpub" style="display: none;" />
+            </button>
             <span v-if="chapter" style="flex-grow: 1; text-align: center;">{{ chapter.title }}</span>
+            <span v-if="!chapter" style="flex-grow: 1;">Upload a <b>*.epub</b> file to get started</span>
             <button v-if="chapter" @click="prev" class="button active-border">
                 <font-awesome-icon :icon="['fas', 'caret-left']" fixed-width />
             </button>
@@ -86,19 +93,53 @@ async function prev() { let p = chapter.value?.prev(); if (p) { chapter.value = 
 </template>
 
 <style>
+#toc {
+    position: relative;
+    display: inline-block;
+}
+#toc-content {
+    display: none;
+    position: absolute;
+    background-color: #383838;
+    min-width: 20rem;
+    height: auto;
+    max-height: 80vh;
+    overflow: scroll;
+    overflow-x: hidden;
+    box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.4);
+    z-index: 1;
+}
+#toc:hover #toc-content {
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+}
+#toc-content > div {
+    display: block;
+    padding: 0.6em .8em;
+    text-wrap: wrap;
+    text-align: left;
+}
+#toc-content > div:hover {
+    background-color: #474747;
+}
+
 .newline {
     color: #474747;
 }
+
 .paragraph {
     display: flex;
     flex-wrap: wrap;
     height: fit-content;
     margin-bottom: 1.5em;
 }
+
 .word {
     display: inline;
     padding: .2em;
 }
+
 .letter {
     display: inline;
     font-size: 1em;
@@ -106,6 +147,7 @@ async function prev() { let p = chapter.value?.prev(); if (p) { chapter.value = 
     color: #9C9C9C;
     user-select: none;
 }
+
 .letter.correct {
     color: #f3f3f3
 }
@@ -150,23 +192,21 @@ async function prev() { let p = chapter.value?.prev(); if (p) { chapter.value = 
     font-size: 1.4rem;
 }
 
-#file-label { /* For some unknown reason the height ends up different from <button> based elements*/
-    padding: 0.43rem 1.0rem;
-}
-
 .button {
     border-radius: 8px;
     padding: 0.5rem 1.0rem;
     font-size: 1rem;
     line-height: normal;
-    background-color: #1a1a1a;
+    background-color: #252525;
     cursor: pointer;
     border: 0;
 }
+
 .active-border {
     border: 1px solid transparent;
     transition: border-color 0.25s;
 }
+
 .active-border:hover {
     border-color: #646cff;
 }
