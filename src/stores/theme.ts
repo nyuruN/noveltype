4d2267a4254ed95cd3b5ce32ecd93842aa841a62
nyuruN <zhangwai.herbst@gmail.com>
2025-05-29 17:09:44 +0200

    chore: mock theme switcher buttons

---
+++ src/stores/theme.ts
@@ -0,0 +1,70 @@
+import { defineStore } from "pinia";
+import { ref, watch } from "vue";
+
+export interface Theme {
+    name: string,
+
+    primaryColor: string,
+    accentColor: string,
+    textColor: string,
+    textDimmedColor: string,
+}
+
+export const Themes: Theme[] = [
+    {
+        name: 'Default',
+        primaryColor: 'hsl(0 0% 30% / var(--opacity))',
+        accentColor: 'hsl(80 70% 55%)',
+        textColor: 'hsl(0 0% 90%)',
+        textDimmedColor: 'hsl(0 0% 60%)',
+    }, {
+        name: 'Red',
+        primaryColor: 'hsl(50 10% 90% / var(--opacity))',
+        accentColor: 'hsl(20 90% 60%)',
+        textColor: 'hsl(0 0% 20%)',
+        textDimmedColor: 'hsl(0 0% 45%)',
+    },
+] as const;
+
+export const useThemeStore = defineStore('theme', () => {
+    let theme = ref(Themes[0])
+    let opacity = ref('')
+    let blur = ref('')
+
+    function $reset() {
+        theme.value = Themes[0]
+        opacity.value = ''
+        blur.value = ''
+    }
+
+    // Watchers
+    watch(theme, (nt) => {
+        document.documentElement.style.setProperty('--primary', String(nt.primaryColor));
+        document.documentElement.style.setProperty('--accent', String(nt.accentColor));
+        document.documentElement.style.setProperty('--text', String(nt.textColor));
+        document.documentElement.style.setProperty('--text-dimmed', String(nt.textDimmedColor));
+    })
+    watch(opacity, (newOpacity) => {
+        document.documentElement.style.setProperty('--opacity', String(newOpacity));
+    })
+    watch(blur, (blurAmount) => {
+        document.documentElement.style.setProperty('--blur-amount', String(blurAmount));
+    })
+
+    // Functions
+    function changeTheme(newTheme: Theme) {
+        theme.value = newTheme
+    }
+
+    return {
+        // States
+        theme,
+        opacity,
+        blur,
+        // Functions
+        $reset,
+        changeTheme,
+    }
+}, {
+    persist: true
+})
--- src/views/ThemesView.vue
+++ src/views/ThemesView.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
+import { useThemeStore, Themes } from '@/stores/theme';
 
+const store = useThemeStore()
 </script>
 
 <template>
     <h1>Themes</h1>
     <div class="container flex" style="margin-top: 2rem;">
+        <template v-for="(theme) in Themes">
+            <button style="cursor: pointer;" :style="{ backgroundColor: theme.primaryColor, color: theme.accentColor }"
+                @click="store.changeTheme(theme)">
+                {{ theme.name }}
+            </button>
+        </template>
     </div>
 </template>
 
@@ -12,5 +20,13 @@
 .container {
     flex-wrap: wrap;
     gap: 1rem;
+}
+
+.container>button {
+    border: none;
+    padding: 0.5rem 1rem;
+    font-size: 1.2em;
+    border-radius: 8px;
+    width: 16rem;
 }
 </style>
